01807206509ae191f781a19a270478e61920a793
lenovo <lenovo@DESKTOP-O95QVK8>
2024-09-22 10:49:53 +0400

    Add project files.

---
+++ TaskManagerClient/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TaskManagerClient
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+        private void InitializeComponent()
+        {
+            txtOutput = new TextBox();
+            btnGetProcesses = new Button();
+            btnKillProcess = new Button();
+            btnRunProcess = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            txtOutput.Location = new Point(13, 45);
+            txtOutput.Margin = new Padding(4, 3, 4, 3);
+            txtOutput.Multiline = true;
+            txtOutput.Name = "txtOutput";
+            txtOutput.ReadOnly = true;
+            txtOutput.ScrollBars = ScrollBars.Vertical;
+            txtOutput.Size = new Size(411, 317);
+            txtOutput.TabIndex = 2;
+            // 
+            // btnGetProcesses
+            // 
+            btnGetProcesses.Location = new Point(302, 368);
+            btnGetProcesses.Margin = new Padding(4, 3, 4, 3);
+            btnGetProcesses.Name = "btnGetProcesses";
+            btnGetProcesses.Size = new Size(122, 60);
+            btnGetProcesses.TabIndex = 3;
+            btnGetProcesses.Text = "Get All";
+            btnGetProcesses.UseVisualStyleBackColor = true;
+            btnGetProcesses.Click += btnGetProcesses_Click;
+            // 
+            // btnKillProcess
+            // 
+            btnKillProcess.Location = new Point(159, 368);
+            btnKillProcess.Margin = new Padding(4, 3, 4, 3);
+            btnKillProcess.Name = "btnKillProcess";
+            btnKillProcess.Size = new Size(124, 60);
+            btnKillProcess.TabIndex = 5;
+            btnKillProcess.Text = "Kill";
+            btnKillProcess.UseVisualStyleBackColor = true;
+            btnKillProcess.Click += btnKillProcess_Click;
+            // 
+            // btnRunProcess
+            // 
+            btnRunProcess.Location = new Point(13, 368);
+            btnRunProcess.Margin = new Padding(4, 3, 4, 3);
+            btnRunProcess.Name = "btnRunProcess";
+            btnRunProcess.Size = new Size(124, 60);
+            btnRunProcess.TabIndex = 7;
+            btnRunProcess.Text = "Post";
+            btnRunProcess.UseVisualStyleBackColor = true;
+            btnRunProcess.Click += btnRunProcess_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(424, 27);
+            button1.TabIndex = 8;
+            button1.Text = "Join";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(448, 440);
+            Controls.Add(button1);
+            Controls.Add(btnRunProcess);
+            Controls.Add(btnKillProcess);
+            Controls.Add(btnGetProcesses);
+            Controls.Add(txtOutput);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Task Manager Client";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnGetProcesses;
+        private System.Windows.Forms.Button btnKillProcess;
+        private System.Windows.Forms.Button btnRunProcess;
+        private Button button1;
+    }
+}
---
+++ TaskManagerClient/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TaskManagerClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        StreamReader reader;
+        StreamWriter writer;
+        private string serverIP = "127.0.0.1";
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+
+
+        private void btnGetProcesses_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("GET");  
+                string jsonResponse = reader.ReadLine();  
+                JArray processes = JArray.Parse(jsonResponse);  
+                txtOutput.Clear();
+                foreach (var process in processes)
+                {
+                    txtOutput.AppendText($"ID: {process["Id"]} - Name: {process["Name"]}\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnKillProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("delete");  
+                MessageBox.Show("All proccess ended.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnRunProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("get");
+                string response = "get";
+                JArray processes = JArray.Parse(response);
+                txtOutput.Text = string.Join(Environment.NewLine, processes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void txtServerIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                try
+                {
+                    string ipAddress = serverIP;
+                    client = new TcpClient(ipAddress, 8888); 
+                    NetworkStream stream = client.GetStream();
+
+                    writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+                    reader = new StreamReader(stream, Encoding.UTF8);
+
+                    MessageBox.Show("Joined Successfully!");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(": " + ex.Message);
+                }
+            
+        }
+    }
+}
